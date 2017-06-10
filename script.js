538660ab192021dbb0ed1ebb4f290b819b259684
VorVitaliy <voronets.vitaliy@gmail.com>
2017-06-10 20:13:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//Первое задание;
+
+var selector = document.querySelector("#item_1").innerHTML;
+var id = document.getElementById('item_1').innerHTML;
+var children = document.body.children[0].children[0].innerHTML;
+
+console.log("First element 'li', selector:", selector);
+console.log("First element 'li', id:", id);
+console.log("First element 'li', children:", children);
+
+
+//Второе задание;
